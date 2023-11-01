9f24548c02e8cb6e290bbdfb85e4109630422967
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-01 19:14:27 +0000

    feat(ui): add initial skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let choice: string;
+</script>
+
+<div>
+	<div>C acha q é doido?</div>
+	<div>
+		<div>
+			<label for="q1">Absolutamente não</label>
+			<input type="radio" name="q1" id="q1" value="q1" bind:group={choice} />
+		</div>
+		<div>
+			<label for="q1">Acho q não</label>
+			<input type="radio" name="q2" id="q2" value="q2" bind:group={choice} />
+		</div>
+		<div>
+			<label for="q1">Sei la</label>
+			<input type="radio" name="q3" id="q3" value="q3" bind:group={choice} />
+		</div>
+		<div>
+			<label for="q1">Acho q sim</label>
+			<input type="radio" name="q4" id="q4" value="q4" bind:group={choice} />
+		</div>
+		<div>
+			<label for="q1">Com certesa</label>
+			<input type="radio" name="q5" id="q5" value="q5" bind:group={choice} />
+		</div>
+	</div>
+</div>
